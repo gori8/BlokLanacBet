b6060518a6a19200d06598dafb2c2bbac77cf526
gori8 <goriantolovic@gmail.com>
2021-07-03 19:47:26 +0200

    ScoresAndQuotas Contract implemented locally

--- DAppContracts/test/betting-tests.js
+++ DAppContracts/test/betting-tests.js
@@ -1,24 +1,36 @@
-const ScoresAndResults = artifacts.require("ScoresAndResults");
+const ScoresAndQuotas = artifacts.require("ScoresAndQuotas");
 const BlokLanacBet = artifacts.require("BlokLanacBet");
 
-contract("ScoresAndResults", (accounts) => {
-  it("score should be 1", async function () {
-    let instance = await ScoresAndResults.deployed();
-    await instance.setScore(web3.utils.asciiToHex("1"));
-    let score = await instance.getScore();
-    assert.equal(score, "1", "Score not set properly");
+contract("ScoresAndQuotas", (accounts) => {
+  it("score should be s1-1", async function () {
+    let instance = await ScoresAndQuotas.deployed();
+    await instance.fulfill(web3.utils.asciiToHex("s-101-105-9460"));
+    let score = await instance.gameScores(9460);
+    assert.equal(score.home, "101", "Score not set properly");
+    assert.equal(score.away, "105", "Score not set properly");
   });
 });
 
-contract("BlokLanacBetIntegration", (accounts) => {
-  it("score should be 1 from BlokLanacBet", () =>
-    ScoresAndResults.deployed()
+contract("ScoresAndQuotas", (accounts) => {
+  it("quotas should be q1.80", async function () {
+    let instance = await ScoresAndQuotas.deployed();
+    await instance.fulfill(web3.utils.asciiToHex("q-180-720-220-9460"));
+    let quotas = await instance.gameQuotas(9460);
+    assert.equal(quotas.one, "180", "Quotas not set properly");
+    assert.equal(quotas.x, "720", "Quotas not set properly");
+    assert.equal(quotas.two, "220", "Quotas not set properly");
+  });
+});
+
+/*contract("BlokLanacBetIntegration", (accounts) => {
+  it("score should be s1 from BlokLanacBet", () =>
+    ScoresAndQuotas.deployed()
       .then((instance) => {
-        instance.setScore(web3.utils.asciiToHex("1"));
+        instance.fulfill(web3.utils.asciiToHex("s1"));
       })
       .then(() => BlokLanacBet.deployed())
       .then((instance) => instance.requestScoresAndResults())
       .then((score) => {
-        assert.equal(score, "1", "Score not requested properly");
+        assert.equal(score, "s1", "Score not requested properly");
       }));
-});
+});*/
